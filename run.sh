a3268b88055ebfcb192e7b4fd4448d26dd844a51
Yangyang Zhao <yangyangzhao@deepglint.com>
2023-05-25 17:40:00 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+python -m pdb main.py --video_file data/left --path results/zed \
+  --camera_params "1055.96826171875, 960.7957763671875, 529.894775390625" --camera_model "SIMPLE_PINHOLE" \
+  --make_video
